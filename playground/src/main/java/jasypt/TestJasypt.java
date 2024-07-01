0467b8caa0b57053463bcdee961d5737e3fb4fe8
kelsonthony <k.thony@gmail.com>
2024-07-01 12:07:20 -0300

    update 3

--- playground/src/main/java/jasypt/TestJasypt.java
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -9,5 +9,8 @@ public class TestJasypt {
         standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
         standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
         standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt("");
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
     }
 }
