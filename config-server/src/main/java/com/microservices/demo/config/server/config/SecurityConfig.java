b6e44c0b7ffe7dacc87550498b3b8fa216795171
kelsonthony <k.thony@gmail.com>
2024-07-08 10:38:31 -0300

    update project with elastic service module

--- config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.microservices.demo.config.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,8 +11,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .antMatchers("/actuator/**")
                 .antMatchers("/encrypt/**")
                 .antMatchers("/decrypt/**");
         super.configure(web);
     }
+//@Override
+//protected void configure(HttpSecurity http) throws Exception {
+//    http
+//       .csrf().disable()
+//       .authorizeRequests()
+//       .antMatchers("/encrypt/**", "/decrypt/**").permitAll()
+//       .anyRequest().authenticated();
+//    }
+
 }
